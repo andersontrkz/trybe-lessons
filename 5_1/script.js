9921dffc6759f506d4e73199e418b03af4e3eb54
Anderson <trkz.anderson@gmail.com>
2021-03-14 23:05:59 -0300

    Lições de manipulação da DOM

---
+++ 5_1/script.js
@@ -0,0 +1,11 @@
+// crie um arquivo JavaScript com o nome de "script.js" na pasta exercises/5_1 e coloque seus conhecimentos de getElementBy e querySelector em prática.
+
+var div = document.getElementsByTagName("div");
+div[0].style.backgroundColor = "pink";
+div[1].style.backgroundColor = "pink";
+div[2].style.backgroundColor = "gray";
+div[3].style.backgroundColor = "gray";
+div[4].style.backgroundColor = "gray";
+
+var header = document.getElementsByTagName("header");
+header[0].style.backgroundColor = "green";
